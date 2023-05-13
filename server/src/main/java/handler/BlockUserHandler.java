7e7c0745d169ebaa5c26851842606ea2d3c5c2e3
Sicheng Liu <scliu0016@outlook.com>
2023-05-13 16:34:58 -0700

    BlockUser

---
+++ server/src/main/java/handler/BlockUserHandler.java
@@ -0,0 +1,28 @@
+package handler;
+
+import dao.AuthDao;
+import dao.UserDao;
+import request.ParsedRequest;
+import response.HttpResponseBuilder;
+
+public class BlockUserHandler implements BaseHandler {
+
+    @Override
+    public HttpResponseBuilder handleRequest(ParsedRequest request) {
+        var res = new HttpResponseBuilder();
+        UserDao userDao = UserDao.getInstance();
+        AuthDao authDao = AuthDao.getInstance();
+        AuthFilter.AuthResult authResult = AuthFilter.doFilter(request);
+        if(!authResult.isLoggedIn){
+            return new HttpResponseBuilder().setStatus(StatusCodes.UNAUTHORIZED);
+        }
+
+        String userId = authResult.userName;
+        String blockedUserId = request.getBody();
+
+        userDao.blockUser(userId, blockedUserId);
+
+        res.setStatus("200 OK");
+        return res;
+    }
+}
