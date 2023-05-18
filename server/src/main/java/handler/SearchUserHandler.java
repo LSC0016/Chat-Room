64f7e0256033986b1079aaeefeb32d86c03f2e08
melany <mmartinez9@mail.sfsu.edu>
2023-05-18 02:37:56 -0700

    search handler file created

---
+++ server/src/main/java/handler/SearchUserHandler.java
@@ -0,0 +1,12 @@
+package handler;
+import com.google.gson.Gson;
+import dao.UserDao;
+import dto.UserDto;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.bson.Document;
+import request.ParsedRequest;
+import response.HttpResponseBuilder; 
+
+public class SearchUserHandler {
+    
+}
