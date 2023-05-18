0082508095a9641987f881f64c861dbb5b108fc6
melany <mmartinez9@mail.sfsu.edu>
2023-05-18 11:34:32 -0700

    searchuser handler

--- server/src/main/java/handler/SearchUserHandler.java
+++ server/src/main/java/handler/SearchUserHandler.java
@@ -1,12 +1,28 @@
 package handler;
-import com.google.gson.Gson;
 import dao.UserDao;
-import dto.UserDto;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.bson.Document;
 import request.ParsedRequest;
 import response.HttpResponseBuilder; 
+import handler.AuthFilter.AuthResult;
+import response.RestApiAppResponse;
 
-public class SearchUserHandler {
+
+public class SearchUserHandler implements BaseHandler {
+     @Override
+        public HttpResponseBuilder handleRequest(ParsedRequest request) {
     
-}
+            // gets username 
+            UserDao userDao = UserDao.getInstance();
+            AuthResult authResult = AuthFilter.doFilter(request);
+            if(!authResult.isLoggedIn){
+              return new HttpResponseBuilder().setStatus(StatusCodes.UNAUTHORIZED);
+            }
+        
+            var filter = new Document("userName", authResult.userName);
+        
+            var res = new RestApiAppResponse<>(true, userDao.query(filter), null);
+            return new HttpResponseBuilder().setStatus("200 OK").setBody(res);
+          }
+        
+    }
+        
